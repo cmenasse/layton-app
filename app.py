7c21c45a3617e09445182aba0ab4034b534c4985
cmenasse <cmenasse@student.42.fr>
2025-05-25 12:24:46 +0200

    add plots

--- app.py
+++ app.py
@@ -80,6 +80,15 @@ with tab2:
     df = data.to_pandas()
     st.dataframe(df)
 
+
+
+    counts = df['picarats'].value_counts().sort_index().reset_index()
+    counts.columns = ['Values', 'Count']
+    fig = px.bar(counts, x='Values', y='Count', title='Picarats distribution')
+    fig.update_xaxes(tickmode='linear', tick0=0, dtick=10)
+    st.plotly_chart(fig)
+
+
     counts = df['picarats'].value_counts().sort_index().reset_index()
     counts.columns = ['Values', 'Count']
 
@@ -93,7 +102,23 @@ with tab2:
     fig.update_xaxes(tickmode='linear', tick0=0, dtick=10)
     st.plotly_chart(fig)
 
-    
+    category_counts = df['category'].value_counts()
+    single_occurrences = category_counts[category_counts < 2].index
+    df['category_grouped'] = df['category'].apply(lambda x: 'Other' if x in single_occurrences else x)
+    grouped_counts = df['category_grouped'].value_counts().reset_index()
+    grouped_counts.columns = ['Category', 'Count']
+
+    fig = px.pie(
+        grouped_counts,
+        values='Count',
+        names='Category',
+        title='Categories',
+        hole=0.4  
+    )
+    st.plotly_chart(fig)
+
+
+
 
 with tab3:
 
